0c01d71c6afb1909783622e9e619c7337cd42f26
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-25 06:14:23 +0000

    Deploy saschanaz/scroll-agnostic-timeline to github.com/saschanaz/scroll-agnostic-timeline.git:gh-pages

---
+++ lib/index.d.ts
@@ -0,0 +1,16 @@
+interface InternalStatus<T> {
+    compare: ((x: T, y: T) => number) | null;
+    max: number;
+}
+export default class ScrollAgnosticTimeline<T extends HTMLElement> extends HTMLElement {
+    private _status;
+    compare: InternalStatus<T>["compare"];
+    max: number;
+    private _removeByInvisibility;
+    constructor();
+    private _findInsertionPositionBinary;
+    private _getVisibleFirstChild;
+    appendChild(newChild: T): T;
+}
+export {};
+export as namespace ScrollAgnosticTimeline;
