fe3bfb0d70de0cc767257e2dc0c12acbb3f4868a
Kagami Sascha Rosylight <saschanaz@outlook.com>
2018-08-27 11:18:10 +0900

    update sample to use beforeautoremove

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,6 +3,16 @@ interface InternalStatus<T> {
     compare: ((x: T, y: T) => number) | null;
     max: number;
 }
+interface BeforeAutoRemoveEventInit<T extends HTMLElement> extends EventInit {
+    elementToBeRemoved: T;
+}
+export declare class BeforeAutoRemoveEvent<T extends HTMLElement> extends Event {
+    elementToBeRemoved: T;
+    constructor(typeArg: string, eventInit: BeforeAutoRemoveEventInit<T>);
+}
+export interface ScrollAgnosticTimelineEventMap extends HTMLElementEventMap {
+    "beforeautoremove": Event;
+}
 export default class ScrollAgnosticTimeline<T extends HTMLElement> extends HTMLElement {
     private _status;
     compare: InternalStatus<T>["compare"];
@@ -12,6 +22,7 @@ export default class ScrollAgnosticTimeline<T extends HTMLElement> extends HTMLE
     private _findInsertionPositionBinary;
     private _getVisibleFirstChild;
     appendChild(newChild: T): T;
+    private _autoRemove;
 }
 export {};
 export as namespace ScrollAgnosticTimeline;
